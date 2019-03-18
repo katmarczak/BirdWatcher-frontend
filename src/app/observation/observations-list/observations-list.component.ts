de852b5098e2e6f50d12f39b57ca9acc859c6526
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2019-03-18 12:08:54 +0100

    add observations list display

--- src/app/observation/observations-list/observations-list.component.ts
+++ src/app/observation/observations-list/observations-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ObservationService } from '../shared/observation.service';
 
 @Component({
   selector: 'app-observations-list',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./observations-list.component.scss']
 })
 export class ObservationsListComponent implements OnInit {
+  observations;
 
-  constructor() { }
+  constructor(private service: ObservationService) { }
 
-  ngOnInit() {
-  }
+  getObservations():void { this.service.getObservations().subscribe(observations => this.observations = observations); }
+
+  ngOnInit() { this.getObservations(); }
 
 }
