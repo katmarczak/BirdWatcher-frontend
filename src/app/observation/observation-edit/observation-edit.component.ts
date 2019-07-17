11f5178bda20b133c90bdec2b7028b6632760890
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2019-07-17 21:04:34 +0200

    add few omitted files from previous commit

---
+++ src/app/observation/observation-edit/observation-edit.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ObservationService } from '../shared/observation.service';
+import { Observation } from '../shared/observation.model';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-observation-edit',
+  templateUrl: './observation-edit.component.html',
+  styleUrls: ['./observation-edit.component.scss']
+})
+export class ObservationEditComponent implements OnInit {
+  observation: Observation;
+
+  constructor(
+    private observationService: ObservationService, 
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.getObservation();
+  }
+
+  getObservation() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.observationService.getObservation(id).subscribe(observation => this.observation = observation);
+  }
+
+  update(observation) {
+    const data = { speciesId: observation.species._id, date: observation.date, exactLocation: observation.exactLocation };
+    this.observationService.editObservation(data).subscribe(
+      (observation) => {
+        this.router.navigate(['/observations', observation._id]);
+    }, (error) => console.log(error));
+  }
+
+}
