409c49efb7e42af7cfa2f945790a57250e8c7d11
Katarzyna Nęcka <katarzyna.necka@gmail.com>
2019-07-17 18:45:00 +0200

    add auth token only when it's available

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -6,7 +6,10 @@ import { Observable } from 'rxjs';
 export class AuthInterceptor implements HttpInterceptor {
      intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
          const token = localStorage.getItem('jwt-token');
-         const authReq = req.clone({ setHeaders: { 'x-auth-token': token }});
-         return next.handle(authReq);
+         if(token) {
+            const authReq = req.clone({ setHeaders: { 'x-auth-token': token }});
+            return next.handle(authReq);
+         }
+         return next.handle(req);
      }
 }
